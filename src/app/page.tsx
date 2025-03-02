9a0e15af3ed31d2cb590cbaa028005853c59f86a
Danny Guzman <dguzman@system-zero.com>
2025-03-01 22:39:30 -0800

    Reapply "Init"
    This reverts commit 5d742547c677dd928c93631830c22e4c93918504.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,93 @@
-import Image from "next/image";
+import { MapsFrontPage } from './components/maps/frontpage';
+import './page.scss';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div className="dashboard">
+
+			{/* Head: Context, Navigation, Quick Actions & Profile */}
+			<div className="head">
+				<div className="container">
+					<div className="icon"><i className="fa fa-clipboard" /></div>
+					<div className="context">
+						<div>Clients</div>
+						<div>Sites</div>
+					</div>
+					<div className="navigation">
+						<div className="item">Home</div>
+						<div className="item">Work Orders</div>
+						<div className="item">Safety</div>
+						<div className="item">Personnel</div>
+						<div className="item">Reports</div>
+					</div>
+					<div className="actions">
+						<div className="icon"><i className="fa fa-envelope" /></div>
+						<div className="icon"><i className="fa fa-bell" /></div>
+					</div>
+					<div className="profile">
+					<div><i className="fa fa-th" /></div>
+						<div><i className="fa fa-user" /></div>
+					</div>
+				</div>
+			</div>
+
+			{/* Body: Main Contents */}
+			<div className="body">
+
+				{/* Welcome Banner */}
+				<div className="welcome">
+					<div className="left">
+						<div className="hello">Welcome back,</div>
+						<div className="notify">You have <span>2</span> new messages and <span>10</span> new notifications.</div>
+					</div>
+					<div className="right">
+						<div className="icons"><i className="fa fa-caret-left" /></div>
+						<div className="dates">Feb 10, 2025 <i className="fa fa-arrows-alt-h" /> Apr 2nd, 2025</div>
+						<div className="icons"><i className="fa fa-caret-right" /></div>
+					</div>
+				</div>
+
+				{/* Content Container */}
+				<div className="container">
+
+					{/* Main Contents */}
+					<div className="main">
+						<div className="bubbles">
+							<div className="item">
+								<div className="icon"><i className="far fa-list-alt" /></div>
+								<div className="label">Open Work Orders</div>
+								<div className="value">27</div>
+								<div className="flip"><i className="far fa-chart-bar" /></div>
+							</div>
+							<div className="item">
+								<div className="icon"><i className="far fa-money-bill-alt" /></div>
+								<div className="label">Cost Summary <span>(02/10/25 - 04/02/25)</span></div>
+								<div className="value">$1989</div>
+							</div>
+							<div className="item">
+								<div className="icon"><i className="far fa-users" /></div>
+								<div className="label">Personnel On Duty</div>
+								<div className="value">39</div>
+							</div>
+						</div>
+						<div className="sites-map"><MapsFrontPage /></div>
+						<div className="sites-list">Site List</div>
+					</div>
+
+					{/* Minimal Contents */}
+					<div className="calendar">
+						<div>Quick News</div>
+						<div>Calendar</div>
+					</div>
+				</div>
+
+			</div>
+
+			{/* Foot */}
+			<div className="foot">
+				<div className="recent">Recent Navigation</div>
+			</div>
+		</div>
+	);
 }
